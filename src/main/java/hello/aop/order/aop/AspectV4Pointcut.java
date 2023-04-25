5e23b469e2acd51db8fc5b046e6792bb8d36e381
sunwooKim <frod5@kakao.com>
2023-04-25 17:33:48 +0900

    스프링 핵심원리2 고급편 - 포인트컷을 따로 객체로 분리하여 참조하는 방식

---
+++ src/main/java/hello/aop/order/aop/AspectV4Pointcut.java
@@ -0,0 +1,48 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+//참고
+//> 스프링 AOP는 AspectJ의 문법을 차용하고, 프록시 방식의 AOP를 제공한다. AspectJ를 직접 사용하는
+//것이 아니다.
+//> 스프링 AOP를 사용할 때는 @Aspect 애노테이션을 주로 사용하는데, 이 애노테이션도 AspectJ가
+//제공하는 애노테이션이다.
+//참고
+//> @Aspect 를 포함한 org.aspectj 패키지 관련 기능은 aspectjweaver.jar 라이브러리가 제공하는
+//기능이다. 앞서 build.gradle 에 spring-boot-starter-aop 를 포함했는데, 이렇게 하면 스프링의
+//AOP 관련 기능과 함께 aspectjweaver.jar 도 함께 사용할 수 있게 의존 관계에 포함된다.
+//> 그런데 스프링에서는 AspectJ가 제공하는 애노테이션이나 관련 인터페이스만 사용하는 것이고, 실제
+//AspectJ가 제공하는 컴파일, 로드타임 위버 등을 사용하는 것은 아니다. 스프링은 지금까지 우리가 학습한
+//것 처럼 프록시 방식의 AOP를 사용한다.
+
+@Slf4j
+@Aspect
+public class AspectV4Pointcut {
+
+    //Pointcut 참조
+
+    @Around("hello.aop.order.aop.Pointcuts.allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}",joinPoint.getSignature());  //joinPoint signature
+        return joinPoint.proceed();
+    }
+
+    @Around("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            log.info("[트랜잭션 시작] {}",joinPoint.getSignature());
+            Object result = joinPoint.proceed();
+            log.info("[트랜잭션 커밋] {}",joinPoint.getSignature());
+            return result;
+        } catch (Exception e) {
+            log.info("[트랜잭션 롤백] {}",joinPoint.getSignature());
+            throw e;
+        } finally {
+            log.info("[리소스 릴리즈] {}",joinPoint.getSignature());
+        }
+    }
+}
---
+++ src/main/java/hello/aop/order/aop/Pointcuts.java
@@ -0,0 +1,17 @@
+package hello.aop.order.aop;
+
+import org.aspectj.lang.annotation.Pointcut;
+
+public class Pointcuts {
+
+    @Pointcut("execution(* hello.aop.order..*(..))")
+    public void allOrder() {}  //Pointcut signature
+
+
+    //타입(클래스 또는 인터페이스) 이름 패턴 *Service
+    @Pointcut("execution(* *..*Service.*(..))")
+    public void allService() {}
+
+    @Pointcut("allOrder() && allService()")
+    public void orderAndService() {}
+}
